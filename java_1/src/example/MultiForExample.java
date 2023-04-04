8f55c60f9f666354f4ad70db74b00e820d466b21
itwill <itwill@DESKTOP-PJPS0RL>
2023-04-04 10:42:37 +0900

    5day

--- java_1/src/example/MultiForExample.java
+++ java_1/src/example/MultiForExample.java
@@ -1,5 +1,7 @@
 package example;
 
+import java.util.Scanner;
+
 public class MultiForExample {
 	public static void main(String[] args) {
 		//★★★★★
@@ -13,7 +15,6 @@ public class MultiForExample {
 			}
 			System.out.println();
 		}
-
 
 		System.out.println("===============================================================");
 		//★★★★★ 
@@ -31,9 +32,7 @@ public class MultiForExample {
 				}
 			}
 			System.out.println();
-		}
-		
-
+		}	
 
 		System.out.println("===============================================================");
 		//★
@@ -48,7 +47,6 @@ public class MultiForExample {
 			}
 			System.out.println();
 		}
-
 
 		System.out.println("===============================================================");
 		//★☆☆☆★
@@ -68,7 +66,104 @@ public class MultiForExample {
 			System.out.println();
 		}
 		
+		System.out.println("===============================================================");
+		//1 1 1 1 1
+        //2 2 2 2 2
+        //3 3 3 3 3
+        //4 4 4 4 4
+        //5 5 5 5 5 
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("===============================================================");
+		//1 2 3 4 5
+        //1 2 3 4 5
+        //1 2 3 4 5
+        //1 2 3 4 5
+        //1 2 3 4 5
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("===============================================================");
+		//2 3 4 5 6
+        //3 4 5 6 7
+        //4 5 6 7 8
+        //5 6 7 8 9
+        //6 7 8 9 10
+		for(int i=1;i<=5;i++) {
+			for(int j=(i+1);j<=(5+i);j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
 
+		System.out.println("===============================================================");
+		//a 1 2 3 4
+        //b c 5 6 7
+        //d e f 8 9
+        //g h i j 10
+		int num=1;
+		char mun='a';
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=5;j++) {
+				if(i-j<0) {
+					System.out.print(num+" ");
+					num++;
+				}
+				else {
+					System.out.print(mun+" ");
+					mun++;
+				}
+			}
+			System.out.println();
+		}
+		
+		System.out.println("===============================================================");
+		//a 1 2 3 4
+        //b c 5 6 7
+        //d e f 8 9
+        //g h i j 10
+		//for문 만으로 작성해보기
+		num=1;
+		mun='a';
+		int j=0;
+		for(int i=1;i<=4;i++) {
+			for(j=1;(i-j)>=0;j++) {
+				System.out.print(mun+" ");
+				mun++;
+			}
+			for(;j<=5;j++) {
+				System.out.print(num+" ");
+				num++;
+			}
+			System.out.println();
+		}
+		
+		System.out.println("===============================================================");
+		//행과 열의 소를 입력받아 출력하는 프로그램
+		//입력: 3 4
+		//출력: 1 2 3 4
+		//      2 4 6 8
+		//      3 6 9 12
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("행과 열을 순서대로 입력하세요>> ");
+		int x=scanner.nextInt();
+		int y=scanner.nextInt();
+		
+		for(int i=1;i<=x;i++) {
+			for(int f=1;f<=y;f++) {
+				System.out.print((f*i)+" ");
+			}
+			System.out.println();
+		}
 		
 		System.out.println("===============================================================");
 	}
