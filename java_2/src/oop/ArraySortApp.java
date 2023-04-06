c57b8affc57414c5d55b7baf5ebb74a5ca2077dc
itwill <itwill@DESKTOP-PJPS0RL>
2023-04-06 10:10:12 +0900

    7day

--- java_2/src/oop/ArraySortApp.java
+++ java_2/src/oop/ArraySortApp.java
@@ -35,7 +35,7 @@ public class ArraySortApp {
 		int[] array= {30,50,10,40,20};
 		System.out.println("정렬 전>> "+Arrays.toString(array));
 		Arrays.sort(array);
-		System.out.println("정렬 후>> "+Arrays.toString(array));
+		System.out.println("정렬 후>> "+Arrays.toString(array)); 
 		
 	}
 }
