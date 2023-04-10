d0584d4257bddcd2e71c40931e9b48b7ed70d9ca
itwill <itwill@DESKTOP-PJPS0RL>
2023-04-10 09:35:04 +0900

    9day

--- java_2/src/oop/RuntimeApp.java
+++ java_2/src/oop/RuntimeApp.java
@@ -3,7 +3,7 @@ package oop;
 public class RuntimeApp {
 	public static void main(String[] args) {
 		//Runtime 클래스로 객체 만들기
-		//생성자가 은닉화 선언되어 있어 new 연산자로 객체 생성 불가능
+		//생성자가 은닉화 선언되어 있어 new 연산자로 객체 생성 불가능=>싱클톤 패턴의 클래스
 		//Runtime runtime= new Runtime();
 		
 		//Runtime.getRuntime(): Runtime 객체를 반환하는 메소드
@@ -12,15 +12,15 @@ public class RuntimeApp {
 		Runtime runtime1=Runtime.getRuntime();
 		Runtime runtime2=Runtime.getRuntime();
 	
-	    System.out.println("runtime1= "+runtime1);
-	    System.out.println("runtime2= "+runtime2);
+	    System.out.println("runtime1= "+runtime1); //[결과]: java.lang.Runtime@6a5fc7f7
+	    System.out.println("runtime2= "+runtime2); //[결과]: java.lang.Runtime@6a5fc7f7
 	    System.out.println("========================================================");
 	    System.out.println("메모리 정리 전 JVM 사용 메모리의 크기 >>");
-	    System.out.println((runtime1.totalMemory()-runtime1.freeMemory())+"Byte");
+	    System.out.println((runtime1.totalMemory()-runtime1.freeMemory())+"Byte"); //메모리 총량-여유 메모리 = 사용 메모리
 	
-	    runtime1.gc();
+	    runtime1.gc(); //Runtime.gc(): 메모리를 정리하는 Garbage Collector를 실행하는 메소드
 	    
 	    System.out.println("메모리 정리 후 JVM 사용 메모리의 크기 >>");
-	    System.out.println((runtime1.totalMemory()-runtime1.freeMemory())+"Byte");
+	    System.out.println((runtime1.totalMemory()-runtime1.freeMemory())+"Byte"); //메모리 총량-여유 메모리 = 사용 메모리
 	}
 }
--- java_2/src/oop/SingletonApp.java
+++ java_2/src/oop/SingletonApp.java
@@ -19,8 +19,12 @@ public class SingletonApp {
 		
 		//싱글톤 클래스는 생성자가 은닉화 선언되어 있어 new 연산자로 생성자를 호출하여 객체 생성 불가능
 		//=>객체를 반환하는 정적 메소드를 호출하여 참조변수에 반환받은 객체를 저장하여 사용
-		Singleton singleton1 = Singleton.getInsance();
-		Singleton singleton2 = Singleton.getInsance();
+		Singleton singleton1 = Singleton.getInsance(); //[결과]:oop.Singleton@3b6eb2ec
+		Singleton singleton2 = Singleton.getInsance(); //[결과]:oop.Singleton@3b6eb2ec
+		//=>두 객체의 주소가 같음=두 변수가 같은 객체임을 나타냄
+		
+		System.out.println("singleton1= "+singleton1);
+		System.out.println("singleton2= "+singleton2);
 		
 		singleton1.display();
 		singleton2.display();
