38d60c5bb5afbb4cbe1763a15ebb4476dfc2413a
itwill <itwill@DESKTOP-PJPS0RL>
2023-04-19 17:39:59 +0900

    16day

--- java_3/src/xyz/itwill/lang/ConsoleCalculateApp.java
+++ java_3/src/xyz/itwill/lang/ConsoleCalculateApp.java
@@ -9,39 +9,56 @@ import java.util.Scanner;
 //=> 형식에 맞지 않는 연산식이 입력된 경우 에러 메세지 출력 후 프로그램 작성
 //=> 입력 연산식에 공백 입력이 가능하도록 처리
 
+//내 코딩의 문제점
+//=> 사칙연산에 사용되는 피연산자가 음수일 경우((ex) -20 -50) 음수 피연산자의 -도 split()메소드에의해 연산자로 인식하여 실행오류 발생
+
 public class ConsoleCalculateApp {
 	public static void main(String[] args) {
 		try {
 			Scanner scanner = new Scanner(System.in);
 			System.out.print("연산식 입력 >> ");
-			String calc = scanner.nextLine();
+			String calc = scanner.nextLine().replace(" ", "");
 			if (calc.indexOf("+") == -1 && calc.indexOf("-") == -1 && calc.indexOf("*") == -1
 					&& calc.indexOf("/") == -1) {
 				System.out.println("[에러]형식에 맞지 않는 연산식이 입력되었으므로 프로그램을 강제 종료합니다.");
 				System.exit(0);
 			}
 
-			calc = calc.replace(" ", "");
 			String[] calcArray = {};
-			if (calc.contains("+")) {
+			/*
+			 * if (calc.contains("+")) { calcArray = calc.split("\\+");
+			 * System.out.println("[결과]" + (Integer.parseInt(calcArray[0]) +
+			 * Integer.parseInt(calcArray[1]))); } else if (calc.contains("-")) { calcArray
+			 * = calc.split("\\-"); System.out.println("[결과]" +
+			 * (Integer.parseInt(calcArray[0]) - Integer.parseInt(calcArray[1]))); } else if
+			 * (calc.contains("*")) { calcArray = calc.split("\\*");
+			 * System.out.println("[결과]" + (Integer.parseInt(calcArray[0]) *
+			 * Integer.parseInt(calcArray[1]))); } else if (calc.contains("/")) { calcArray
+			 * = calc.split("\\/"); System.out.println("[결과]" +
+			 * (Integer.parseInt(calcArray[0]) / Integer.parseInt(calcArray[1]))); }
+			 */
+
+			if (calc.indexOf("+") != -1) {
 				calcArray = calc.split("\\+");
 				System.out.println("[결과]" + (Integer.parseInt(calcArray[0]) + Integer.parseInt(calcArray[1])));
-			} else if (calc.contains("-")) {
+			} else if (calc.indexOf("-") != -1) {
 				calcArray = calc.split("\\-");
 				System.out.println("[결과]" + (Integer.parseInt(calcArray[0]) - Integer.parseInt(calcArray[1])));
-			} else if (calc.contains("*")) {
+			} else if (calc.indexOf("*") != -1) {
 				calcArray = calc.split("\\*");
 				System.out.println("[결과]" + (Integer.parseInt(calcArray[0]) * Integer.parseInt(calcArray[1])));
-			} else if (calc.contains("/")) {
+			} else if (calc.indexOf("/") != -1) {
 				calcArray = calc.split("\\/");
 				System.out.println("[결과]" + (Integer.parseInt(calcArray[0]) / Integer.parseInt(calcArray[1])));
 			}
 
 			scanner.close();
 		} catch (ArithmeticException e) {
-			System.out.println("[에러]연산식에 올바르지 않은 수식이 입력되었습니다.");
+			System.out.println("[에러]0으로 나눌 수 없습니다.");
+		} catch (NumberFormatException e) {
+			System.out.println("[에러]연산식에 숫자가 아닌 값이 입력되었습니다.");
 		} catch (Exception e) {
-			System.out.println("[에러]프로그램에 예기치 못한 오류가 발생하였습니다.");
+			System.out.println("[에러]프로그램에 예기치 못한 오류가 발생하였습니다. 프로그램을 강제 종료합니다.");
 		}
 	}
 }
