78d0d788aeb33bd20090b41227f07eb5dc6f739f
itwill <itwill@DESKTOP-PJPS0RL>
2023-04-24 14:29:34 +0900

    19day

--- java_3/src/xyz/itwiil/awt/WindowAdapterApp.java
+++ java_3/src/xyz/itwiil/awt/WindowAdapterApp.java
@@ -6,6 +6,7 @@ import java.awt.event.WindowEvent;
 
 
 //프레임의 [닫기] 버튼을 누른 경우 프로그램을 종료하는 기능의 프로그램 작성
+//=> 프레임에서는 WindowEvent 발생 - WindowAdapter 클래스를 상속받은 자식클래스로 이벤트 처리
 
 //Adapter 클래스를 상속받은 자식클래스(이벤트 처리 클래스)는 필요한 메소드만 오버라이드 선언
 //Adapter 클래스: Listener 인터페이스 중 추상메소드가 2개 이상 선언된 경우 Listener 인터페이스 대신 이벤트 처리를 제공하는 클래스
@@ -34,12 +35,12 @@ public class WindowAdapterApp extends Frame {
 		new WindowAdapterApp("윈도우 이벤트");
 	}
 	
-	/*
+	
 	public class WindowEventHandleTwo extends WindowAdapter {
 		@Override
 		public void windowClosing(WindowEvent e) {
 			System.exit(0);
 		}
 	} 
-	*/
+
 }
--- java_3/src/xyz/itwill/swing/SwingApp.java
+++ java_3/src/xyz/itwill/swing/SwingApp.java
@@ -3,11 +3,19 @@ package xyz.itwill.swing;
 
 import javax.swing.JFrame;
 
-//javax.swing 패키지의 클래스를 이용하여 GUI 프로그램을 작성하는 방법
+//javax.swing 패키지의 클래스를 이용하여 GUI 프로그램을 작성하는 방법-AWT와 다른점
 
 //1.java.awt 패키지의 컴퍼넌트 또는 컨테이너 관련 클래스 대신 javax.swing 패키지의 컴퍼넌트와 컨테이너 관련 클래스를 사용하여 UI 구현
 //=>AWT 컴퍼넌트(컨테이너) 관련 클래스 이름 앞에 J를 붙이면 SWING 컴퍼넌트와 동일
 //  ex) Frame(AWT) >> JFrame(SWING), Button(AWT) >> JButton(SWING)
+
+//2. 프레임의 [닫기]를 누른 경우 동작되는 기능을 기본적으로 제공
+//=>JFrame.setDefaultCloseOperation(int operation) 메소드를 호출하여 프레임의 [닫기]를 누른 경우 동작되는 기능을 변경 가능
+//=> operation 매개변수에서는 WindowConstants 클래스의 상수 전달
+//=> DO_NOTHING_ON_CLOSE
+//=> HIDE_ON_CLOSE
+//=> DISPOSE_ON_CLOSE
+//=> EXIT_ON_CLOSE 
 
 public class SwingApp extends JFrame {
 	private static final long serialVersionUID = 1L;
