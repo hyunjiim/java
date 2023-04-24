43c68fcdccaffff44df167827d5554ae9e5d1a33
itwill <itwill@DESKTOP-PJPS0RL>
2023-04-24 11:35:05 +0900

    19day

--- java_3/src/xyz/itwiil/awt/EventAnonymousHandleApp.java
+++ java_3/src/xyz/itwiil/awt/EventAnonymousHandleApp.java
@@ -5,12 +5,12 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Frame;
 
-import xyz.itwiil.awt.EventInnerHandleApp.EventHandle;
 
-public class EventAnnoymousHandleApp extends Frame {
+//=>이벤트 처리 클래스를 익명의 내부클래스(Anonymous InnerClass)로 작성
+public class EventAnonymousHandleApp extends Frame {
 	private static final long serialVersionUID = 1L;
 	
-	public EventAnnoymousHandleApp(String title) {
+	public EventAnonymousHandleApp(String title) {
 		super(title);
 		
 		setLayout(new FlowLayout());
@@ -23,6 +23,6 @@ public class EventAnnoymousHandleApp extends Frame {
 	}
 	
 	public static void main(String[] args) {
-		new EventAnnoymousHandleApp("이벤트처리");
+		new EventAnonymousHandleApp("이벤트처리");
 	}
 }
