5e06e22c622f3162a1ee5c5befeee35ac89466d5
itwill <itwill@DESKTOP-PJPS0RL>
2023-04-24 11:40:32 +0900

    19day

--- java_3/src/xyz/itwiil/awt/EventInnerHandleApp.java
+++ java_3/src/xyz/itwiil/awt/EventInnerHandleApp.java
@@ -9,6 +9,8 @@ import java.awt.event.ActionListener;
 
 //EventSourceHandleApp >> EventInnerHandleApp 
 //이유: 컴퍼넌트의 수가 많아질 경우 내부클래스로 작성하는 방법이 유지보수에 유리
+//=>가장 좋은 방법
+
 //[EXIT]버튼을 누른 경우 프로그램을 종료하는 기능의 프로그램 작성
 //=>이벤트 처리 클래스를 디자인 클래스의 내부 클래스로 작성
 //=>내부클래스(이벤트 처리 클래스)는 외부클래스(디자인 클래스)의 필드와 메소드를 접근제한자에 상관없이 사용 가능
@@ -38,8 +40,7 @@ public class EventInnerHandleApp extends Frame {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
-			
+			System.exit(0);
 		}
 		
 	}
--- java_3/src/xyz/itwiil/awt/EventSourceHandleApp.java
+++ java_3/src/xyz/itwiil/awt/EventSourceHandleApp.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionListener;
 //EventHandleApp의 문제점(이벤트 처리 메소드에서 디자인 클래스의 컴퍼넌트를 사용할 수 없음) 보완
 //[EXIT]버튼을 누른 경우 프로그램을 종료하는 기능의 프로그램 작성
 //=>디자인 클래스와 이벤트 처리 클래스를 하나의 클래스로 작성
+//=>만들기 쉽지만 유지보수가 어려움
 public class EventSourceHandleApp extends Frame implements ActionListener {
 	private static final long serialVersionUID = 1L;
 	
