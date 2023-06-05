56e2d62c8132d9bf4d17e589cb8f78e22481772b
user <user@BOOK-LCCUUB9T5H>
2023-06-05 09:30:43 +0900

    oop-05

---
+++ practice/src/oop_01/CarApp.java
@@ -0,0 +1,45 @@
+package oop_01;
+
+//oop-05
+
+//클래스로 객체를 생성하는 방법
+//형식) 클래스명 참조변수 = new 클래스명();
+//=> new 연산자로 클래스의 기본 생성자를 호출하여 객체를 생성하고 생성된 객체의 메모리
+//	주소를 참조변수에 저장
+// 참조변수에 저장된 객체는 . 연산자를 사용하여 필드 또는 메소드에 이용해 프로그램 작성
+
+//Car 클래스를 객체로 생성하여 작성된 프로그램
+public class CarApp {
+	public static void main(String[] args) {
+		//Car 클래스 객체를 생성하여 참조변수에 저장
+		//=> 하나의 클래스로 서로 다른 객체를 여러개 생성 가능
+		//=> 클래스는 객체를 생성하기 위한틀(Template)
+		//=> 객체를 생성하면 객체의 필드에서는 기본값(숫자형: 0, 논리형: false,
+		//	참조형: null)이 초기값으로 자동 저장
+		Car carOne = new Car(); //modelName=null, engineStatus=false, currentSpeed=0
+		Car carTwo = new Car(); //modelName=null, engineStatus=false, currentSpeed=0
+		Car carThree = new Car(); //modelName=null, engineStatus=false, currentSpeed=0
+		
+		System.out.println("carOne = "+carOne);
+		System.out.println("carTwo = "+carTwo);
+		System.out.println("carThree = "+carThree);
+		System.out.println("=============================================================");
+		//참조변수.필드명: 참조변수에 저장된 객체가 . 연산자를 사용하여 필드에 접근
+		//=> 객체의 필드에는 기본값이 초기값으로 자동 저장
+		//문제점)객체를 사용하여 필드에 직접적인 접근을 허용하면 필드에 비정상적인 값 저장 가능
+		//해결법)클래스 작성시 필드를 은닉화 처리하여 선언 - 은닉화 선언된 필드에 접근할 경우 에러 발생
+		
+		//carOne.modelName = "싼타페"; - 객체의 필드값 변경 - 필드가 은닉화 처리되어 에러 발생
+		
+		carOne.setModelName("싼타페"); //필드값을 변경하는 Setter 메소드 호출
+		
+		//객체의 필드값을 반환하는 Getter 메소드 호출 - 반환받은 필드값 사용(출력)
+		System.out.println("첫 번째 자동차 모델명 = "+carOne.getModelName());
+		System.out.println("첫 번째 자동차 엔진상태 = "+carOne.isEngineStatus());
+		System.out.println("첫 번째 자동차 현재속도 = "+carOne.getCurrentSpeed());
+		System.out.println("=============================================================");
+		System.out.println("=============================================================");
+		System.out.println("=============================================================");
+		System.out.println("=============================================================");
+	}
+}
