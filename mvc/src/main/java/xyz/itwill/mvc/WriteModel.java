32123b675dfcc8fd1907fca0529becc76b367ab4
user <user@BOOK-LCCUUB9T5H>
2023-07-13 11:12:16 +0900

    72day

--- mvc/src/main/java/xyz/itwill/mvc/WriteModel.java
+++ mvc/src/main/java/xyz/itwill/mvc/WriteModel.java
@@ -10,6 +10,9 @@ import xyz.itwill.dto.UserinfoDTO;
 import xyz.itwill.exception.ExistsUserinfoException;
 import xyz.itwill.service.UserinfoService;
 
+//클라이언트가 [/write.do]로 요청한 경우 객체로 생성될 모델 역활의 클래스
+// => 회원정보를 전달받아 USERINFO 테이블에 회원정보로 삽입하고 [/loginform.do]로 
+//리다이렉트 이동하기 위한 정보가 저장된 ActionForward 객체 반환
 public class WriteModel implements Action {
 
 	@Override
@@ -18,7 +21,7 @@ public class WriteModel implements Action {
 		ActionForward actionForward=new ActionForward();
 		UserinfoDTO userinfo=null;
 		try {
-			if(request.getMethod().equals("GET")){
+			if(request.getMethod().equals("GET")) {
 				throw new Exception();
 			}
 			
@@ -44,7 +47,7 @@ public class WriteModel implements Action {
 			
 			actionForward.setForward(false);
 			actionForward.setPath(request.getContextPath()+"/loginform.do");
-		}catch (ExistsUserinfoException e) {
+		} catch (ExistsUserinfoException e) {
 			//아이디가 중복된 경우 발생되는 예외에 대한 예외처리 명령 작성
 			request.setAttribute("message", e.getMessage());
 			request.setAttribute("userinfo", userinfo);
@@ -55,9 +58,7 @@ public class WriteModel implements Action {
 			actionForward.setForward(false);
 			actionForward.setPath(request.getContextPath()+"/error.do");
 		}
-		return null;
+		return actionForward;
 	}
-
-	
 
 }
