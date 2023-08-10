a4d262971a05c9413df6ed2365ba3e5804180454
jirun <jirun@192.168.1.185>
2023-08-10 16:04:14 +0900

    복습

--- spring/src/main/java/xyz/itwill07/aop/EmailSendBean.java
+++ spring/src/main/java/xyz/itwill07/aop/EmailSendBean.java
@@ -21,5 +21,20 @@ import lombok.Setter;
 // => 메일 서버(Mail Server) : 메일을 송수신하는 서비스를 제공하는 컴퓨터
 // => SMTP(Simple Message Transfer Protocol - 25) 서비스로 메일을 보내고 POP3(Post Office Protocol 3 - 110)
 //서비스나 IMAP(Internet Message Access Protocol - 143) 서비스로 메일을 받아 사용자에게 전달
+
+//POP3 : 하나의 기기에서 메일을 확인, 한번만 전달 => 스마트폰이나 다른 기기에서도 메일을 확인해야할 필요성 증다
+// => IMAP : 여러 기기에서 메일 확인 가능
+// => 사용하기 위해 메일 서버와 DNS 서버 구현
+
 public class EmailSendBean {
+	//JavaMailSender 객체를 저장하기 위한 필드 선언 - Setter Injection을 위해 @Setter 어노테이션 사용
+	// => JavaMailSender 객체 : SMTP 서비스를 제공하는 서버의 정보를 저장하기 위한 객체 - 메일 서버?
+	@Setter
+	private JavaMailSender javaMailSender;
+	
+	//메일을 전송하는 메소드
+	// => 메일을 받는 사람의 이메일 주소, 제목, 내용을 매개변수로 전달받아 저장
+	// => 메일을 받는 사람의 이메일 주소를 반환
+	
+	
 }
