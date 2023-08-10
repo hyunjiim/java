2acd3164db05be8be71d3840b55d0279da4334dd
jirun <jirun@192.168.1.185>
2023-08-10 10:44:48 +0900

    90day

--- spring/src/main/java/xyz/itwill10/controller/TilesController.java
+++ spring/src/main/java/xyz/itwill10/controller/TilesController.java
@@ -1,5 +1,6 @@
 package xyz.itwill10.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //TilesView 클래스 : 요청 처리 메소드의 반환값(ViewName)을 제공받아 다수의 JSP 문서가 결합된
@@ -7,10 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //1. TilesView 관련 라이브러리(tiles-extras 라이브러리 - 의존관계에 의해 다른 tiles-어쩌구 라이브러리들도 사용 가능)를
 //프로젝트에 빌드 처리 - 메이븐 : pom.xml
 //2. 요청 처리 메소드의 반환값(ViewName)을 제공받아 응답할 템플릿 페이지 설정
-// => TilesView 프로그램의 환경설정파일을 설정 - /WEB/INF/spring/appServlet/tiles.xml
+// => TilesView 프로그램의 환경설정파일을 설정 - /WEB/INF/spring/appServlet/tiles.xml - 다른곳에 작성해도 되지만 여기에 작성하면 접근성이 더 좋음!
 //3. Front Controller(DispatcherServlet 클래스)가 요청 처리 메소드의 반환값(ViewName)을 제공받아
 //TilesView 클래스를 이용하여 응답 처리되도록 Spring Bean Configuration File(servlet-context.xml) 설정
 // => InternalResourceViewResolver 클래스보다 TilesView 클래스가 먼저 실행되도록 우선 순위 설정
+@Controller
 public class TilesController {
 	//@RequestMapping("/") : 최상위 패키지?를 호출했을 때
 	@RequestMapping("/")
